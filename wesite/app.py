d827f85e560dbf101e2c1945a5b8a99d47be22dd
sewwandiWM <it20298494@my.sliit.lk>
2022-10-24 15:36:11 +0530

    changes

--- wesite/app.py
+++ wesite/app.py
@@ -1,9 +1,14 @@
 from pydoc import render_doc
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')##, methods=["POST", "GET"])
 def index():
+    ##if request.method == "POST":
+        ##total_income = request.form['income']
+        ##total_children=request.form['children']
+        
+        
     return render_template("index.html")
     
 
